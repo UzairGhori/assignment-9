6b2717d0a6ac90006d78c27d0f07efed15cf5a85
UzairGhori <ughori435@gmail.com>
2024-11-14 22:45:48 +0500

    assignment-9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,112 +2,112 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="bg-white">
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      <div className="flex bg-blue-700 justify-around items-center w-auto  h-24 ">
+
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+         src="/images/logoG.jpg"
+         alt="Logo"
+         width={100}
+         height={100} 
+         className="pt-16"
+         />
+
+        <h2 className="text-2xl text-white font-serif ">Tution Free Education Program On Latest Technologies</h2>
+    
+      <div>
+        <ul className="flex text-2xl space-x-3 text-white">
+          <li>Home</li>
+          <li>Apply</li>
+          <li>Job</li>
+          <li>Result</li>
+          <li>Courses</li>
+        </ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
-    </main>
-  );
+      <div className="flex pt-10 ">
+        <div className="pl-8">
+          <h1 className="whitespace-nowrap text-center text-[2rem] font-extrabold leading-10 tracking-wider  text-main sm:text-5xl sm:leading-none md:text-left text-blue-800 lg:text-6xl  mt-0">Governor Sindh</h1>
+          <h1 className="whitespace-nowrap text-center text-[1.5rem] font-normal leading-[2rem] tracking-wider text-main sm:text-4xl sm:leading-[3rem] md:text-left text-blue-800 lg:text-[2.5rem]">Kamran Khan Tessori</h1>
+          <h1 className="whitespace-nowrap text-center text-[1.5rem] font-semibold leading-[2rem] tracking-wider text-sub sm:text-4xl sm:leading-[3rem] md:text-left text-blue-400">
+            Certified Cloud <br />
+            Applied Generative AI <br />
+            Engineer (GenEng)
+          </h1>
+          <p className="my-5 whitespace-nowrap text-center text-[1.25rem] font-extrabold text-main sm:text-3xl text-blue-900 md:text-left">Earn up to $5,000 / month</p>
+          <p className="my-5 w-full text-center text-[2.25rem] font-bold tracking-wider text-main sm:text-3xl md:w-[80%] md:text-left text-blue-900">Now admissions are <br /> open in Hyderabad</p>
+          <div className="mb-7 mt-5 flex flex-col items-center sm:mt-10 sm:flex-row sm:gap-5 md:mb-0">
+            <a className="w-full text-white bg-blue-900 rounded-md md:w-auto" href="/Apply">
+              <button className="w-full rounded-md bg-main py-3 text-center text-sm font-semibold tracking-widest text-white transition-all hover:translate-y-1 sm:py-4 sm:text-base md:w-52">APPLY NOW</button>
+            </a>
+            <div className="mt-4 flex w-full flex-col items-center sm:mt-0">
+              <div className="flex-col text-center text-xl tracking-widest text-main sm:text-3xl">
+                <div className="w-40 font-extrabold text-blue-900">562,143</div>
+              </div>
+              <div className="text-center text-1xl font-semibold text-blue-900 tracking-wider text-main sm:mb-0 sm:text-sm">Accepted <br /> Applications</div>
+            </div>
+          </div>
+        </div>
+       </div>
+
+       <div className="flex flex-col items-center -mt-96  sm:flex-row  md:flex-col md:gap-0">
+       <Image  
+      src="/Images/governorsindh.png"  
+      alt="Giaic"  
+      loading="lazy"  
+      width={1300}  
+      height={900}  
+      decoding="async"  
+      data-nimg="1"  
+      className="w-full h-auto max-w-[700px] -mt-36 justify-items-end ml-auto"  
+/>
+    </div>
+  
+    <div className="flex justify-around p-5 mt-6 ">
+        
+          <Image
+          src="/Images/p1.jpg"
+          alt="Giaic"
+          loading="lazy"
+          width={100}
+          height={100}
+          decoding="async"
+          data-nimg="1"
+          className="w-full h-auto max-w-[800px] justify-items-start m-5 ml-auto" 
+          
+          />
+        <Image
+          src="/Images/p2.jpg"
+          alt="Giaic"
+          loading="lazy"
+          width={100}
+          height={100}
+          decoding="async"
+          data-nimg="1"
+          className="w-full h-auto max-w-[800px] justify-items-start m-5 ml-auto" 
+          
+          />
+          <Image
+          src="/Images/p3.jpg"
+          alt="Giaic"
+          loading="lazy"
+          width={100}
+          height={100}
+          decoding="async"
+          data-nimg="1"
+          className="w-full h-auto max-w-[800px] justify-items-start m-5 ml-auto" 
+          
+          />
+      </div>
+      <footer>
+        <div className="text-center text-2xl bg-gray-500 text-white p-5">
+          &copy; 2024 Governor Sindh. All rights reserved.
+        </div>
+      </footer>
+    </div>
+      
+      
+  
+   );
 }
